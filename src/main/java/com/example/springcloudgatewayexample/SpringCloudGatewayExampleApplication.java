350e56ea558579061af1f7698b7f1fe856f2c522
holosievo@gmail.com <gshockprotection>
2019-05-02 13:16:05 +0200

    deleted code under comments

--- src/main/java/com/example/springcloudgatewayexample/SpringCloudGatewayExampleApplication.java
+++ src/main/java/com/example/springcloudgatewayexample/SpringCloudGatewayExampleApplication.java
@@ -9,30 +9,10 @@ import reactor.core.publisher.Mono;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-// @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SpringCloudGatewayExampleApplication /** extends WebSecurityConfigurerAdapter */{
+public class SpringCloudGatewayExampleApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudGatewayExampleApplication.class, args);
 	}
-/**
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		// Disable CSRF (cross site request forgery)
-		http.cors().and().csrf().disable();
-
-		// No session will be created or used by spring security
-		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-		// Entry points
-		http.authorizeRequests()
-
-				// Disallow everything else..
-				.anyRequest().authenticated();
-		// http.apply();
-		http.httpBasic();
-	}
- */
-
 	@Bean
 	public KeyResolver testResolver(){
 		return exchange -> Mono.just(exchange.getSession().subscribe().toString());
