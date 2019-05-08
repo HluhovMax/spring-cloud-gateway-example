48d5267c16eb27971e2b9f62fbf567e1314ec42b
holosievo@gmail.com <gshockprotection>
2019-05-08 13:03:54 +0200

    first commit to dev branch

--- src/main/java/com/example/springcloudgatewayexample/SpringCloudGatewayExampleApplication.java
+++ src/main/java/com/example/springcloudgatewayexample/SpringCloudGatewayExampleApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import reactor.core.publisher.Mono;
 
@@ -13,12 +15,19 @@ import reactor.core.publisher.Mono;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class SpringCloudGatewayExampleApplication{
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudGatewayExampleApplication.class, args);
-	}
-	@Bean
-	public KeyResolver testResolver(){
-		return exchange -> Mono.just(exchange.getSession().subscribe().toString());
-	}
+public class SpringCloudGatewayExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGatewayExampleApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return null;
+    }
+
+    @Bean
+    public KeyResolver testResolver() {
+        return exchange -> Mono.just(exchange.getSession().subscribe().toString());
+    }
 }
